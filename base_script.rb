7c16a68a4d17330933dd1ae901a25fdd619419a0
Chris Aga <chrisaga.dev@gmail.com>
2021-06-23 04:24:55 -0600

    Map to scrap_issue

--- base_script.rb
+++ base_script.rb
@@ -1,1 +1,7 @@
-issues = RubyweeklyScrapper.scrap_index("https://rubyweekly.com/issues")
+require 'rubyweekly_scrapper'
+
+@issues = RubyweeklyScrapper.scrap_index("https://rubyweekly.com/issues")
+
+@scrap_issues = @issues.map do |issue|
+  RubyweeklyScrapper.scrap_issue(issue.url)
+end
